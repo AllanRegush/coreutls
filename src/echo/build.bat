5564bbdf10b006a7b4fa528114a50d1b915855b2
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-03-24 21:37:20 -0600

    fix(echo): str length

--- src/echo/build.bat
+++ src/echo/build.bat
@@ -3,5 +3,5 @@
 mkdir ..\build
 pushd ..\build
 echo $pwd
-cl -Zi ..\echo\main.cpp user32.lib
+cl -Zi ..\echo\echo.cpp user32.lib
 popd
---
+++ src/echo/echo.cpp
@@ -0,0 +1,29 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+inline 
+int strlen(char *str)
+{
+    int count = 0;
+    for(char *char_ptr = str; *char_ptr != '\0'; ++char_ptr)
+    {
+        count++;
+    }
+    return count;
+}
+
+
+int main(int argc, char **argv)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    for(int i = 1; i < argc; i++)
+    {
+        int Length = strlen(argv[i]);
+        WriteConsole(hConsole, argv[i], Length, 0, 0);
+        if(i + 1 < argc) {
+            WriteConsole(hConsole, " ", 1, 0, 0);
+        }
+    }
+    CloseHandle(hConsole);
+    return 0;
+}
